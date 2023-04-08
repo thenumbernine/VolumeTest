01056e3d0c0730dc640659648bab4bf54b640507
Christopher Moore <christopher.e.moore@gmail.com>
2023-04-08 10:29:10 +0800

    profiling

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "Profiler/Profiler.h"
 #include "Common/Macros.h"		//crand
 #include "Common/Sequence.h"	//make_index_range
 #include "Tensor/Tensor.h"
@@ -5,9 +6,123 @@
 #include <stdlib.h>
 #include <iostream>
 
+
+
+namespace Tensor {
+
+//inner product is going slow
+// because read iterator is going slow
+template<typename A, typename B>
+requires IsBinaryTensorOp<A,B>
+typename A::Scalar innerProfile(A const & a, B const & b) {
+	PROFILE();	// #1 offender.  gcc: 15 seconds.  clang: 3 seconds.
+	auto i = a.begin();
+	auto sum = a(i.index) * b(i.index);
+	for (++i; i != a.end(); ++i) {
+		sum += a(i.index) * b(i.index);
+	}
+	return sum;
+}
+
+template<int num, typename A, typename B>
+requires IsInteriorOp<num, A, B>
+auto interiorProfile(A const & a, B const & b) {
+#if 0
+	return contractN<A::rank-num,num>(outer(a,b));
+#else
+	PROFILE();
+	using S = typename A::Scalar;
+	if constexpr (A::rank == num && B::rank == num) {
+		PROFILE();
+		// rank-0 i.e. scalar result case
+		static_assert(std::is_same_v<typename A::dimseq, typename B::dimseq>);	//thanks to the 3rd requires condition
+		return innerProfile(a,b);
+	} else {
+		PROFILE();
+		using R = typename A
+			::template ReplaceScalar<B>
+			::template RemoveIndexSeq<Common::make_integer_range<int, A::rank-num, A::rank+num>>;
+		static_assert(num != 1 || std::is_same_v<R, decltype(contract<A::rank-1,A::rank>(outer(a,b)))>);
+		static_assert(std::is_same_v<R, decltype(contractN<A::rank-num,num>(outer(a,b)))>);
+		static_assert(R::rank == A::rank + B::rank - 2 * num);
+		return R([&](typename R::intN i) -> S {
+			PROFILE();	// #3 offender
+			auto ai = [&]<int ... j>(std::integer_sequence<int, j...>) constexpr -> typename A::intN {
+				//PROFILE();	//clang can't put PROFILE in constexpr lambdas
+				return typename A::intN{(j < A::rank-num ? i[j] : 0)...};
+			}(std::make_integer_sequence<int, A::rank>{});
+			auto bi = [&]<int ... j>(std::integer_sequence<int, j...>) constexpr -> typename B::intN {
+				//PROFILE();	//clang can't put PROFILE in constexpr lambdas
+				return typename B::intN{(j < num ? 0 : i[j + A::rank-2*num])...};
+			}(std::make_integer_sequence<int, B::rank>{});
+
+			//TODO instead use A::dim<A::rank-num..A::rank>
+			S sum = {};
+#if 0
+			template<typename B>
+			struct InteriorRangeIter {
+				template<int i> constexpr int getRangeMin() const { return 0; }
+				template<int i> constexpr int getRangeMax() const { return B::dims().template dim<i>; }
+			};
+			for (auto k : RangeIteratorInner<num, InteriorRangeIter<B>>(InteriorRangeIter<B>())) {
+#else
+			for (auto k : RangeObj<num, false>(vec<int, num>(), B::dims().template subset<num, 0>())) {
+				PROFILE();	// #2 offender
+#endif
+				std::copy(k.s.begin(), k.s.end(), ai.s.begin() + (A::rank - num));
+				std::copy(k.s.begin(), k.s.end(), bi.s.begin());
+				sum += a(ai) * b(bi);
+			}
+			return sum;
+		});
+	}
+#endif
+}
+
+template<typename A>
+requires IsSquareTensor<A>
+auto hodgeDualProfile(A const & a) {
+	PROFILE();
+	static constexpr int rank = A::rank;
+	static constexpr int dim = A::template dim<0>;
+	static_assert(rank <= dim);
+	using S = typename A::Scalar;
+	if constexpr (dim == 1 && rank == 1) {	// very special case:
+		PROFILE();
+		return a[0];
+	} else if constexpr (dim == 2) {	// TODO this condition isn't needed if you merge asym with asymR
+		PROFILE();
+		return interiorProfile<rank>(a, asym<S, dim>(1)) / (S)constexpr_factorial(rank);
+	} else {
+		PROFILE();
+		auto e = [&](){
+			PROFILE();
+			return asymR<S, dim, dim>(1);
+		}();
+		auto div = [&](){
+			PROFILE();
+			return (S)constexpr_factorial(rank);
+		}();
+		auto ae = [&](){
+			PROFILE();
+			return interiorProfile<rank>(a, e);
+		}();
+		auto dual = [&](){
+			PROFILE();
+			return ae / div;
+		}();
+		return dual;
+	}
+}
+
+}
+
+
+
+
 using real = double;
 
-constexpr size_t maxdim = 9;	// 10 is too much for release mode.  8 or 9 is too much for debug mode.
+constexpr size_t maxdim = 4;	// 10 is too much for release mode.  8 or 9 is too much for debug mode.
 
 template<int i, int dim>
 struct WedgeNTimes {
@@ -24,21 +139,39 @@ struct WedgeNTimes<dim-1, dim> {
 	}
 };
 
-
 // sdim = simplex-dimension.  
 // for the first case it will match ws.dim<0> == ws.dim<1> (since ws.rank == 2)
 // for the rest, ws.dim<0> > sdim, so we gotta pass sdim
 template<size_t sdim>	
 void testVolume(auto const & ws) {
-	auto w = WedgeNTimes<0, sdim>::go(ws);
+	PROFILE();
+	std::cout << "ws: " << ws << std::endl;
+	auto w = [&](){
+		PROFILE();
+		return WedgeNTimes<0, sdim>::go(ws);
+	}();
 //	std::cout << "w = " << sdim << "-simplex in " << sdim << " dimensions:" << std::endl;
-	//auto wexp = w.expand();
-	//std::cout << wexp << std::endl;
-//	std::cout << "*w: " << w.dual() << std::endl;
-	//std::cout << "*w expanded (should match): " << wexp.dual() << std::endl;
-	auto wInner = w.wedge(w.dual()).dual();
+	auto wDual = [&](){
+		PROFILE();
+		return Tensor::hodgeDualProfile(w);
+	}();
+//	auto wexp = w.expand();
+//	std::cout << wexp << std::endl;
+//	std::cout << "*w: " << wDual << std::endl;
+//	std::cout << "*w expanded (should match): " << wexp.dual() << std::endl;
+	auto wInnerForm = [&](){
+		PROFILE();
+		return w.wedge(wDual);
+	}();
+	auto wInner = [&](){
+		PROFILE();
+		return Tensor::hodgeDualProfile(wInnerForm);
+	}();
 //	std::cout << "*(w ∧ *w): " << wInner << std::endl;
-	std::cout << "√(*(w ∧ *w)): " << sqrt(wInner) << std::endl;
+	[&](){
+		PROFILE();
+		std::cout << "√(*(w ∧ *w)): " << sqrt(wInner) << std::endl;
+	}();
 	// frob will match inner in rank-1 norms only
 //	auto wFrob = wexp.lenSq();
 //	std::cout << "|w|_frob: " << wFrob << std::endl;	// Frobenius norm
@@ -143,6 +276,7 @@ struct TestSimplexDimForDim<sdim, stopdim, stopdim> {
 
 template<size_t dim>
 auto randomVec() {
+	PROFILE();
 	auto w = Tensor::vec<real, dim>([](int i) { return 10. * crand(); });
 	std::cout << "generating " << dim << "-dimensional random vector: " << w << std::endl;
 	return w;
@@ -150,6 +284,7 @@ auto randomVec() {
 
 template<size_t sdim>
 void testSimplexDim() {
+	PROFILE();
 	std::cout << std::endl;
 	std::cout << "testing simplex of dimension " << sdim << std::endl;
 
@@ -186,9 +321,12 @@ void testSimplexDim() {
 }
 
 int main() {
+	PROFILE_BEGIN_FRAME()
+	PROFILE();
 	srand(time(nullptr));
 	// for constexpr (size_t i = 1; i <= maxdim; ++i) {
 	[]<auto ... i>(std::index_sequence<i...>) constexpr {
 		(testSimplexDim<i>(), ...);
 	}(Common::make_index_range<1, maxdim+1>{});
+	PROFILE_END_FRAME()
 }
