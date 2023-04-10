d9bad46aab3c3dfacde4e788d70aaa3d6c7167a6
Christopher Moore <christopher.e.moore@gmail.com>
2023-04-10 10:13:56 +0800

    performance-testing the inner product turned into unit-testing the constructors

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,136 @@
-#include "Profiler/Profiler.h"
 #include "Common/Macros.h"		//crand
 #include "Common/Sequence.h"	//make_index_range
 #include "Tensor/Tensor.h"
 #include <sys/time.h>
 #include <stdlib.h>
 #include <iostream>
+#include <cassert>
 
+#if 0
+#include "Profiler/Profiler.h"
+#else
+#define PROFILE_BEGIN_FRAME()
+#define PROFILE_END_FRAME()
+#define PROFILE()
+#endif
 
+#include <sstream>
 
 namespace Tensor {
+
+// TODO move this to Tensor?  and clean it up maybe?
+template<typename N, int i>
+void serializeIndex(auto & o) {
+	using Ni = std::remove_pointer_t<std::tuple_element_t<i, N>>;
+	o << " " << Ni::tensorxStr();
+};
+template<typename T>
+std::string tensorxName(T const & t) {
+	using N = typename T::NestedPtrTensorTuple;
+	std::ostringstream o;
+	[]<auto ... i>(auto & o, std::index_sequence<i...>) constexpr {
+		(serializeIndex<N,i>(o), ...);
+	}(o, std::make_index_sequence<std::tuple_size_v<N>>{});
+	return o.str();
+}
+
+template<int b>
+constexpr int ipow(int a) {
+	if constexpr (b == 0) {
+		return 1;
+	} else if constexpr (b == 1) {
+		return a;
+	} else {
+		return a * ipow<b-1>(a);
+	}
+}
 
 //inner product is going slow
 // because read iterator is going slow
 template<typename A, typename B>
-requires IsBinaryTensorOp<A,B>
-typename A::Scalar innerProfile(A const & a, B const & b) {
-	PROFILE();	// #1 offender.  gcc: 15 seconds.  clang: 3 seconds.
-	auto i = a.begin();
-	auto sum = a(i.index) * b(i.index);
-	for (++i; i != a.end(); ++i) {
-		sum += a(i.index) * b(i.index);
+requires (
+	IsBinaryTensorOp<A,B> || 				// ... with matching rank
+	(!is_tensor_v<A> && !is_tensor_v<B>)	// ... or two scalars
+)
+auto innerProfile(A const & a, B const & b) {
+	PROFILE();
+	if constexpr (!is_tensor_v<A> || !is_tensor_v<B>) {	// two scalars:
+		PROFILE();
+		return a * b;
+	} else {
+		PROFILE();
+		using AS = typename A::Scalar;
+		using BS = typename B::Scalar;
+		using RS = decltype(AS() * BS());
+//std::cout << typeid(A).name() <<  << std::endl;
+//how to serialize the tensorx type?
+// NestedPtrTensorTuple
+std::cout << tensorxName(a) << " inner " << tensorxName(b) << std::endl;
+//std::cout << "sized " << a.localCount << " x " << b.localCount << std::endl;
+		/*
+		vec
+		ident
+		zero
+		sym
+		asym
+		symR
+		asymR
+		
+		how can I optimize this?
+		if A is sym and B is sym (or A is asym and B is asym) then we can double up the symmetric indexes (same with symR) ... but how many times?
+		*/
+	
+		if constexpr (is_zero_v<A> || is_zero_v<B>) {
+		//if A or B is a zero then return zero.
+			PROFILE();
+			return RS{};
+		} else if constexpr (is_ident_v<A> || is_ident_v<B>) {
+		//if A is an ident or B is an ident then return the trace of inners of a and b
+			PROFILE();
+			RS sum = {};
+			for (int i = 0; i < a.localDim; ++i) {
+				sum += innerProfile(a(i,i), b(i,i));
+			}
+			return sum;
+		} else if constexpr (
+			std::is_same_v<A, B>	// TODO test if matching up to scalar types
+			&& (is_asym_v<A> || is_asymR_v<A>)	// ... then ... we can short circuit ...
+			&& sizeof(A) == sizeof(typename A::Scalar)	// TODO how about optimizing for all cases?
+			// this would involve iterate/sum-of-products over all localCount storage
+			// with products weighted by some factor
+		) {
+			PROFILE();
+			// TODO wrong
+			// inner(a,b) produces the correct result
+			//return inner(a,b);
+			// a.s[0] * b.s[0] produces the incorrect result
+			return innerProfile(a.s[0], b.s[0]) * (RS)constexpr_factorial(A::localRank);
+				// * (RS)ipow<A::localRank>(A::localDim);
+				// * (RS)consteval_nChooseR(A::localRank, A::localDim);
+#if 0
+		// if *any* neighboring indexes is of a sym(R) in A and asym(R) in B (or vice versa) then the result is zero (same with symR)
+		// i.e. a_i1_..._[ik_i{k+1}] b^i1^...^(ik^i{k+1}) = 0
+		// i.e. for rank-k, iterator i=0..k-2, 
+		//		if the nesting-index in A of i == nesting-index of i+1 
+		//	... and same with nesting-index in B of i == nesting-index of i+1
+		//	... and is_sym(R) of that nesting in A and is_asym(R) of that nesting in B (or vice versa)
+		//	... then return 0
+		} else if constexpr (
+			false
+		) {
+#endif	
+		} else {
+		//otherwise old fashioned
+			PROFILE();
+			//ok asymR inner asymR is going to iterate over its same component many times ...
+			auto i = a.begin();
+			auto sum = a(i.index) * b(i.index);
+			for (++i; i != a.end(); ++i) {
+				sum += a(i.index) * b(i.index);
+			}
+			return sum;
+		}
 	}
-	return sum;
 }
 
 template<int num, typename A, typename B>
@@ -67,7 +176,7 @@ auto interiorProfile(A const & a, B const & b) {
 			for (auto k : RangeIteratorInner<num, InteriorRangeIter<B>>(InteriorRangeIter<B>())) {
 #else
 			for (auto k : RangeObj<num, false>(vec<int, num>(), B::dims().template subset<num, 0>())) {
-				PROFILE();	// #2 offender
+				PROFILE();	// #2 offender ... #1 offender with inner optimized for asymR
 #endif
 				std::copy(k.s.begin(), k.s.end(), ai.s.begin() + (A::rank - num));
 				std::copy(k.s.begin(), k.s.end(), bi.s.begin());
@@ -122,34 +231,25 @@ auto hodgeDualProfile(A const & a) {
 
 using real = double;
 
-constexpr size_t maxdim = 5;	// 10 is too much for release mode.  8 or 9 is too much for debug mode.
+constexpr size_t maxdim = 8;	// 10 is too much for release mode.  8 or 9 is too much for debug mode.
 
-template<int i, int dim>
-struct WedgeNTimes {
-	static constexpr auto go(auto const & v) {
-		if constexpr (i < dim-1) {
-			return v[i].wedge(
-				WedgeNTimes<i+1, dim>::go(v)
-			);
-		} else {
-			return v[dim-1];
-		}
-	}
-};
-
-// sdim = simplex-dimension.  
 // for the first case it will match ws.dim<0> == ws.dim<1> (since ws.rank == 2)
-// for the rest, ws.dim<0> > sdim, so we gotta pass sdim
+// for the rest, ws.dim<0> < ws.dim<1>
 void testVolume(auto const & ws) {
-	constexpr int sdim = std::decay_t<decltype(ws)>::template dim<0>;
-	
+	// ws is tensor (sdim, dim)
 	PROFILE();
 //	std::cout << "ws: " << ws << std::endl;
 	auto w = [&](){
 		PROFILE();
-		return WedgeNTimes<0, sdim>::go(ws);
+		return ws.wedgeAll();
 	}();
+	// w is tensor(dim) wedge ... wedge tensor(dim) (sdim-times)
+	// so w is tensor(dim, dim, ...) totally-antisymmetric, rank<=sdim
+	// so it'll go from 'dim' unique elements down to 1 unique element
+	// number of unique elements is (dim ncr sdim)
+//	constexpr int sdim = std::decay_t<decltype(ws)>::template dim<0>;
 //	std::cout << "w = " << sdim << "-simplex in " << sdim << " dimensions:" << std::endl;
+	std::cout << "w: " << w << std::endl;	
 	auto wDual = [&](){
 		PROFILE();
 		return Tensor::hodgeDualProfile(w);
@@ -292,9 +392,67 @@ int main() {
 	PROFILE_BEGIN_FRAME()
 	PROFILE();
 	srand(time(nullptr));
+#if 0
 	// for constexpr (size_t i = 1; i <= maxdim; ++i) {
 	[]<auto ... i>(std::index_sequence<i...>) constexpr {
 		(testSimplexDim<i>(), ...);
 	}(Common::make_index_range<1, maxdim+1>{});
+#else
+
+	// turns out rank 1x1x...xN ctors doesn't' work ... unless it's 1x1x ... x1
+#if 1
+	// TODO why isn't this working?
+	using real2 = Tensor::tensor<real, 2>;
+	auto i = real2{1,3};
+	auto j = real2{2,4};
+	using real1x2 = Tensor::tensor<real, 1, 2>;
+#if 1	// works
+	auto ii = real1x2{i};
+	auto jj = real1x2{j};
+#endif
+#if 0	// fails
+	auto ii = real1x2(i);	// doesn't work
+	auto jj = real1x2(j);
+#endif
+	assert(innerProfile(ii, jj) == 14);
+	assert(innerProfile(Tensor::tensor<real, 1, 3>{{1,3,2}}, Tensor::tensor<real, 1, 3>{{2,4,3}}) == 20);
+#endif
+
+
+
+	// rank-1 vectors
+	assert(innerProfile(Tensor::tensor<real, 1>{3}, Tensor::tensor<real, 1>{4}) == 12);
+	assert(innerProfile(Tensor::tensor<real, 2>{1,3}, Tensor::tensor<real, 2>{2,4}) == 14);
+	assert(innerProfile(Tensor::tensor<real, 3>{1,3,2}, Tensor::tensor<real, 3>{2,4,3}) == 20);
+
+	// rank-2 dense vectors
+	assert(innerProfile(Tensor::tensor<real, 1, 1>{{3}}, Tensor::tensor<real, 1, 1>{{5}}) == 15);
+	assert(innerProfile(Tensor::tensor<real, 2, 1>{{1},{3}}, Tensor::tensor<real, 2, 1>{{2},{4}}) == 14);
+
+	assert(innerProfile(Tensor::tensor<real, 2, 2>{{1, 2},{3, 4}}, Tensor::tensor<real, 2, 2>{{4, 3},{2, 1}}) == 20);
+	
+	assert(innerProfile(Tensor::tensor<real, 3, 1>{{1},{3},{2}}, Tensor::tensor<real, 3, 1>{{2},{4},{3}}) == 20);
+	
+	assert(innerProfile(Tensor::tensor<real, 3, 2>{{1,2},{3,4},{2,5}}, Tensor::tensor<real, 3, 2>{{2,1},{4,2},{3,3}}) == 45);
+	assert(innerProfile(Tensor::tensor<real, 3, 3>{{1,2,3},{4,5,6},{7,8,9}}, Tensor::tensor<real, 3, 3>{{9,8,7},{6,5,4},{3,2,1}}) == 165);
+	
+
+
+	// rank-3 dense
+
+	// rank-2 ident
+
+	// rank-2 sym*sym
+	// rank-2 sym*asym
+	// rank-2 asym*sym
+	// rank-2 asym*asym
+
+	// rank-3 same
+
+	// rank-4 same
+
+	// rank-4 ident outer rank-2
+
+#endif
 	PROFILE_END_FRAME()
 }
